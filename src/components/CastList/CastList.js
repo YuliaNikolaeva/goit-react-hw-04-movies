dda65ec89952e6c0e691d1c3fce28d4a0ba4aa3e
Yulia Nikolaeva <y.nikolaeva@rndev.org>
2020-08-04 21:21:47 +0300

    Fix styles of components 04

--- src/components/CastList/CastList.js
+++ src/components/CastList/CastList.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import s from './CastList.module.css'
 
 const CastList = ({children}) => {
-    return <ul>{children}</ul>;
+    return <ul className={s.castList}>{children}</ul>;
 };
 
 export default CastList;
